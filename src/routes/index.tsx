5082e1c981fd095824bbf30cb6dd16582d793e7f
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2025-11-11 22:15:04 +0500

    fix: import `fs` fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,3 @@
-// src/routes/index.tsx
 import * as fs from "node:fs/promises";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
@@ -6,10 +5,7 @@ import { createServerFn } from "@tanstack/react-start";
 const filePath = "count.txt";
 
 async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, "utf-8").catch(() => "0"),
-    10,
-  );
+  return parseInt(await fs.readFile(filePath, "utf-8").catch(() => "0"), 10);
 }
 
 const getCount = createServerFn({
@@ -22,7 +18,7 @@ const updateCount = createServerFn({ method: "POST" })
   .inputValidator((d: number) => d)
   .handler(async ({ data }) => {
     const count = await readCount();
-    await fs.promises.writeFile(filePath, `${count + data}`);
+    await fs.writeFile(filePath, `${count + data}`);
   });
 
 export const Route = createFileRoute("/")({
