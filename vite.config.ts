abec1e0c09d62e193f6d39d63489c30a399ad7ba
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2025-11-11 19:54:55 +0500

    chore: add Cloudflare & Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
@@ -11,6 +12,7 @@ const config = defineConfig({
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
+    cloudflare({ viteEnvironment: { name: "client" as "client" | "ssr" } }),
     tailwindcss(),
     tanstackStart(),
     nitro({ preset: "bun" }),
